a87c99dfda529fd5bd0360c25b336d733aa446dc
Alisina Bahadori <alisina.bm@gmail.com>
2025-02-23 12:09:12 -0500

    Release 0.0.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LiveDashboardLogger.MixProject do
   use Mix.Project
 
-  @version "0.0.2"
+  @version "0.0.3"
   @source_url "https://github.com/alisinabh/live_dashboard_logger"
 
   def project do
